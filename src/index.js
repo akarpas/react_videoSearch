04ed2ecbc1aa2f4801ab4d0e5f349b7f213db6cc
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-06-10 18:32:56 +0200

    implement video list component and video details component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react'; // CREATES AND MANAGES COMPONENTS
+import React, { Component } from 'react'; // CREATES AND MANAGES COMPONENTS
 import ReactDOM from 'react-dom'; // INTERACTS WITH ACTUAL DOM
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; // IMPORT COMPONENT SEARCH BAR
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = "keyhere";
 
 // Create new component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }; // array since it will contain a list of videos (i.e. objects)
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
 
 // Put it in the dom
 ReactDOM.render(<App />, document.querySelector('.container'));
